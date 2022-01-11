0730f8c7be58545f0cfc68ab5e23d077f87e9c61
ue64683 <gilles.diacon@sbb.ch>
2022-01-11 18:34:41 +0100

    gdi: shop: use white text color + improve html

--- shop.php
+++ shop.php
@@ -24,7 +24,7 @@
         ?>
 
         <div id="events" class="pt-3 min-vh-100" style="background-image: url('image/shop_bg.jpg'); background-size: cover">
-            <div class="container-fluid w-75">
+            <div class="container-fluid w-75 text-white">
                 <h1><?php echo $activeMenuTitle?></h1>
 
                 <p>Vous trouverez sûrement votre bonheur dans notre magasin de jeux. Pour offrir ou pour votre plaisir,
@@ -34,95 +34,93 @@
 
                 <div class="row">
                     <div class="col">
-                        <div class="card">
-                            <div class="col">
-                                <div class="card-header p-0 bg-transparent">
-                                    <a class="btn btn-sm w-100" data-bs-toggle="collapse" href="#collapseFilter">
-                                        Filtres
-                                    </a>
-                                </div>
-                                <div class="card-body collapse" id="collapseFilter">
-                                    <form>
-                                        <div class="row mb-3">
-                                            <label for="title" class="col-sm-1 col-form-label text-end pe-3">Titre</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" class="form-control" id="title">
-                                            </div>
+                        <div class="card bg-transparent border-white">
+                            <div class="card-header p-0">
+                                <a class="btn btn-sm w-100 text-white" data-bs-toggle="collapse" href="#collapseFilter">
+                                    Filtres
+                                </a>
+                            </div>
+                            <div class="card-body collapse" id="collapseFilter">
+                                <form>
+                                    <div class="row mb-3">
+                                        <label for="title" class="col-sm-1 col-form-label text-end pe-3">Titre</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="title">
                                         </div>
-                                        <div class="row mb-3">
-                                            <div class="col">
-                                                <div class="row">
-                                                    <label for="player" class="col-sm-2 col-form-label text-end pe-3">Joueurs(s)</label>
-                                                    <div class="col-sm-5">
-                                                        <select id="player" class="form-select">
-                                                            <option selected></option>
-                                                            <?php foreach ($shopPlayerService->getShopPlayers() as $key => $value) {
-                                                                echo "<option value='" . $value->value . "'>" . $value->text . "</option>";
-                                                            } ?>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col">
-                                                <div class="row">
-                                                    <label for="maxDuration" class="col-sm-3 col-form-label text-end pe-3">Durée maximum</label>
-                                                    <div class="col-sm-5">
-                                                        <select id="player" class="form-select">
-                                                            <option selected></option>
-                                                            <?php foreach ($shopDurationService->getShopDurations() as $key => $value) {
-                                                                echo "<option value='" . $value->value . "'>" . $value->text . "</option>";
-                                                            } ?>
-                                                        </select>
-                                                    </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col">
+                                            <div class="row">
+                                                <label for="player" class="col-sm-2 col-form-label text-end pe-3">Joueurs(s)</label>
+                                                <div class="col-sm-5">
+                                                    <select id="player" class="form-select">
+                                                        <option selected></option>
+                                                        <?php foreach ($shopPlayerService->getShopPlayers() as $key => $value) {
+                                                            echo "<option value='" . $value->value . "'>" . $value->text . "</option>";
+                                                        } ?>
+                                                    </select>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="row mb-3">
-                                            <label for="ages" class="col-sm-1 col-form-label text-end pe-3 pt-0">Age</label>
-                                            <div class="col-sm-10">
-                                                <?php foreach ($shopAgeService->getShopAges() as $key => $value) {
-                                                    echo "<div class='form-check form-check-inline'>";
-                                                    echo "<input class='form-check-input' type='checkbox' id='" . $value->value . "' name='ages' value='" . $value->value . "'>";
-                                                    echo "<label class='form-check-label' for='" . $value->value . "'>" . $value->text . "</label>";
-                                                    echo "</div>";
-                                                } ?>
+                                        <div class="col">
+                                            <div class="row">
+                                                <label for="maxDuration" class="col-sm-3 col-form-label text-end pe-3">Durée maximum</label>
+                                                <div class="col-sm-5">
+                                                    <select id="player" class="form-select">
+                                                        <option selected></option>
+                                                        <?php foreach ($shopDurationService->getShopDurations() as $key => $value) {
+                                                            echo "<option value='" . $value->value . "'>" . $value->text . "</option>";
+                                                        } ?>
+                                                    </select>
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="row mb-3">
-                                            <label for="categories" class="col-sm-1 col-form-label text-end pe-3 pt-0">Catégorie</label>
-                                            <div class="col-sm-10">
-                                                <?php foreach ($shopCategoryService->getShopCategories() as $key => $value) {
-                                                    echo "<div class='form-check form-check-inline'>";
-                                                    echo "<input class='form-check-input' type='checkbox' id='" . $value->value . "' name='catgeories' value='" . $value->value . "'>";
-                                                    echo "<label class='form-check-label' for='" . $value->value . "'>" . $value->text . "</label>";
-                                                    echo "</div>";
-                                                } ?>
-                                            </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <label for="ages" class="col-sm-1 col-form-label text-end pe-3 pt-0">Age</label>
+                                        <div class="col-sm-10">
+                                            <?php foreach ($shopAgeService->getShopAges() as $key => $value) {
+                                                echo "<div class='form-check form-check-inline'>";
+                                                echo "<input class='form-check-input' type='checkbox' id='" . $value->value . "' name='ages' value='" . $value->value . "'>";
+                                                echo "<label class='form-check-label' for='" . $value->value . "'>" . $value->text . "</label>";
+                                                echo "</div>";
+                                            } ?>
                                         </div>
-                                        <div class="row mb-3">
-                                            <label for="them" class="col-sm-1 col-form-label text-end pe-3 pt-0">Thème</label>
-                                            <div class="col-sm-10">
-                                                <?php foreach ($shopThemeService->getShopThemes() as $key => $value) {
-                                                    echo "<div class='form-check form-check-inline'>";
-                                                    echo "<input class='form-check-input' type='checkbox' id='" . $value->value . "' name='themes' value='" . $value->value . "'>";
-                                                    echo "<label class='form-check-label' for='" . $value->value . "'>" . $value->text . "</label>";
-                                                    echo "</div>";
-                                                } ?>
-                                            </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <label for="categories" class="col-sm-1 col-form-label text-end pe-3 pt-0">Catégorie</label>
+                                        <div class="col-sm-10">
+                                            <?php foreach ($shopCategoryService->getShopCategories() as $key => $value) {
+                                                echo "<div class='form-check form-check-inline'>";
+                                                echo "<input class='form-check-input' type='checkbox' id='" . $value->value . "' name='catgeories' value='" . $value->value . "'>";
+                                                echo "<label class='form-check-label' for='" . $value->value . "'>" . $value->text . "</label>";
+                                                echo "</div>";
+                                            } ?>
                                         </div>
-                                        <div class="row mb-3">
-                                            <label for="them" class="col-sm-1 col-form-label text-end pe-3 pt-0">Type</label>
-                                            <div class="col-sm-10">
-                                                <?php foreach ($shopTypeService->getShopTypes() as $key => $value) {
-                                                    echo "<div class='form-check form-check-inline'>";
-                                                    echo "<input class='form-check-input' type='checkbox' id='" . $value->value . "' name='types' value='" . $value->value . "'>";
-                                                    echo "<label class='form-check-label' for='" . $value->value . "'>" . $value->text . "</label>";
-                                                    echo "</div>";
-                                                } ?>
-                                            </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <label for="them" class="col-sm-1 col-form-label text-end pe-3 pt-0">Thème</label>
+                                        <div class="col-sm-10">
+                                            <?php foreach ($shopThemeService->getShopThemes() as $key => $value) {
+                                                echo "<div class='form-check form-check-inline'>";
+                                                echo "<input class='form-check-input' type='checkbox' id='" . $value->value . "' name='themes' value='" . $value->value . "'>";
+                                                echo "<label class='form-check-label' for='" . $value->value . "'>" . $value->text . "</label>";
+                                                echo "</div>";
+                                            } ?>
                                         </div>
-                                    </form>
-                                </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <label for="them" class="col-sm-1 col-form-label text-end pe-3 pt-0">Type</label>
+                                        <div class="col-sm-10">
+                                            <?php foreach ($shopTypeService->getShopTypes() as $key => $value) {
+                                                echo "<div class='form-check form-check-inline'>";
+                                                echo "<input class='form-check-input' type='checkbox' id='" . $value->value . "' name='types' value='" . $value->value . "'>";
+                                                echo "<label class='form-check-label' for='" . $value->value . "'>" . $value->text . "</label>";
+                                                echo "</div>";
+                                            } ?>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
