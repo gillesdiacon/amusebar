53795a9910f90e389967643a82f921befb7ffffd
ue64683 <gilles.diacon@sbb.ch>
2022-01-21 17:19:53 +0100

    gdi: keep new images for home sub-menu commented

--- nav.php
+++ nav.php
@@ -32,16 +32,24 @@
                         <?php if('index'==$activeMenuPage) { ?>
                             <ul class="navbar-nav flex-row">
                                 <li class="nav-item">
-                                    <a class="nav-link h5 py-0" href="#concept">Concept</a>
+                                    <a class="nav-link h5 py-0" href="#concept">Concept
+<!--                                        <img src="image/homeSubMenu/concept.svg" alt="Concept" width="160px" />-->
+                                    </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link h5 py-0" href="#charte">Charte</a>
+                                    <a class="nav-link h5 py-0" href="#charte">Charte
+<!--                                        <img src="image/homeSubMenu/convention.svg" alt="Charte"/>-->
+                                    </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link h5 py-0" href="#team">Equipe</a>
+                                    <a class="nav-link h5 py-0" href="#team">Equipe
+<!--                                        <img src="image/homeSubMenu/team.svg" alt="Equipe"/>-->
+                                    </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link h5 py-0" href="#timetable">Horaires</a>
+                                    <a class="nav-link h5 py-0" href="#timetable">Horaires
+<!--                                        <img src="image/homeSubMenu/timetable.svg" alt="Horaires"/>-->
+                                    </a>
                                 </li>
                             </ul>
                         <?php } ?>
