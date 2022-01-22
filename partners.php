2dde39dedfc2e8021e611b6caaf989a6792dbb96
ue64683 <gilles.diacon@sbb.ch>
2022-01-22 18:05:10 +0100

    gdi: add new partner + improve layout

--- partners.php
+++ partners.php
@@ -2,20 +2,25 @@
     <div class="row">
         <div class="col-lg-3 d-none d-lg-block">&nbsp;</div>
         <div class="col">
-            <div class="h1 py-5 text-center">Partenaires</div>
+            <div class="h1 pt-5 text-center">Partenaires</div>
             <div class="row">
-                <div class="col">
-                    <div class="h1 text-center">Ludesco</div>
+                <div class="col py-5 px-3">
+                    <div class="h1 text-center text-nowrap">Ludesco</div>
                 </div>
 
-                <div class="col">
-                    <div class="h1 text-center">Escape Infinity</div>
+                <div class="col py-5 px-3">
+                    <div class="h1 text-center text-nowrap">Escape Infinity</div>
                 </div>
 
-                <div class="col">
-                    <div class="h1 text-center">(Clientis)</div>
+                <div class="col py-5 px-3">
+                    <div class="h1 text-center text-nowrap">B12 Communication</div>
+                </div>
+
+                <div class="col py-5 px-3">
+                    <div class="h1 text-center text-nowrap">(Clientis)</div>
                 </div>
             </div>
         </div>
+        <div class="col-lg-3 d-none d-lg-block">&nbsp;</div>
     </div>
 </div>
