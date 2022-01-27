4da7a10365b2422fc29a68c70b1bbbae929d8f32
ue64683 <gilles.diacon@sbb.ch>
2022-01-27 17:16:39 +0100

    gdi: add partner logos

--- partners.php
+++ partners.php
@@ -1,23 +1,23 @@
-<div id="partner" class="<?php if('index'==$activeMenuPage){ echo 'pt-8'; }else{ echo 'pt-3'; } ?> min-vh-100 container-fluid" style="background-image: url('image/bg/partner-filtre.png'); background-size: cover">
+<div id="partner" class="<?php if('concept'==$activeMenuPage){ echo 'pt-8'; }else{ echo 'pt-3'; } ?> min-vh-100 container-fluid">
     <div class="row">
         <div class="col-lg-3 d-none d-lg-block">&nbsp;</div>
         <div class="col">
             <div class="h1 pt-5 text-center">Partenaires</div>
-            <div class="row">
-                <div class="col py-5 px-3">
-                    <div class="h1 text-center text-nowrap">Ludesco</div>
+            <div class="row justify-content-center">
+                <div class="col-lg-3 text-center">
+                     <!-- <div class="h1 text-center text-nowrap">Ludesco</div> -->
+                    <img src="image/partner/ludesco.png" height="80px">
                 </div>
-
-                <div class="col py-5 px-3">
-                    <div class="h1 text-center text-nowrap">Escape Infinity</div>
+                <div class="col-lg-3 text-center">
+                    <div class="h4 text-nowrap pt-3">Escape Infinity</div>
                 </div>
-
-                <div class="col py-5 px-3">
-                    <div class="h1 text-center text-nowrap">B12 Communication</div>
+                <div class="col-lg-3 text-center">
+                    <!-- <div class="h1 text-center text-nowrap">(Clientis)</div> -->
+                    <img src="image/partner/clientis.png" height="80px">
                 </div>
-
-                <div class="col py-5 px-3">
-                    <div class="h1 text-center text-nowrap">(Clientis)</div>
+                <div class="col-lg-10 text-center">
+                    <!-- <div class="h1 text-center text-nowrap">B12 Communication</div> -->
+                    <img src="image/partner/b12.png" height="40px">
                 </div>
             </div>
         </div>
