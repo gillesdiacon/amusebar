4c761f9be239e8e12fbcde91be874bd002169917
ue64683 <gilles.diacon@sbb.ch>
2022-01-27 18:25:54 +0100

    gdi: improve home layout on laptop

--- index.php
+++ index.php
@@ -16,25 +16,27 @@ require_once("init.php");
 require_once("nav.php");
 ?>
 
-<div id="concept" class="pt-8 container-fluid amuseBar-bg-cream">
-    <div class="row text-center bg-light amuseBar-bg-cream2">
-        <a href="bar.php" class="col p-4 text-dark">
+<div class="pt-10 container-fluid amuseBar-bg-bar amuseBar-bg-cream">
+    <div class="">
+    <div class="row justify-content-center text-center amuseBar-bg-home2">
+        <a href="bar.php" class="col-lg-4 p-4 text-dark border-bottom">
             <div class="h1 fw-bold amuseBar-text-bar">Le Bar à Jeux</div>
             <div class="">1000 jeux de société pour jouer seul, entre amis ou en famille</div>
             <div class="">Venez découvrir cette magnifique collection et rencontrer nos sommeliers du jeu</div>
         </a>
     </div>
-    <div class="row text-center">
-        <a href="catering.php" class="col p-4 text-dark">
+    <div class="row justify-content-center text-center amuseBar-bg-home1">
+        <a href="catering.php" class="col-lg-4 p-4 text-dark border-bottom">
             <div class="h1 fw-bold amuseBar-text-catering">Restauration</div>
             <div class="">Pour régaler vos parties passionnées et le plaisir de vos papilles</div>
         </a>
     </div>
-    <div class="row text-center bg-light amuseBar-bg-cream2">
-        <a href="shop.php" class="col p-4 text-dark">
+    <div class="row justify-content-center text-center amuseBar-bg-home2">
+        <a href="shop.php" class="col-lg-4 p-4 text-dark">
             <div class="h1 fw-bold amuseBar-text-shop">Le magasin</div>
             <div class="">Pour découvrir les jeux avant de pouvoir les acheter</div>
         </a>
+    </div>
     </div>
 </div>
 
