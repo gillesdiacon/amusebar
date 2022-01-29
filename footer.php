863ece0f34975bfc1209b6d81198d4f18e8064a3
ue64683 <gilles.diacon@sbb.ch>
2022-01-29 10:48:05 +0100

    gdi: improve footer layout

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-            <footer id="footer" class="<?php if('index'!=$activeMenuPage) echo 'footer' ?> mt-auto mt-5">
-                <div class="container-fluid w-75">
-                    <div class="row">
+            <footer id="footer" class="<?php if('index'!=$activeMenuPage) echo 'footer' ?> mt-3">
+                <div class="container-fluid px-5">
+                    <div class="row px-5">
                         <div class="col text-start">
                             <p class="text-muted p-1 m-0">L&apos;AMUSE BAR&nbsp;&copy;&nbsp;2021</p>
                         </div>
