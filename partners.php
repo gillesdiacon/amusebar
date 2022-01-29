530a7fd8b3bf5e0ad8237aff138a04ed0b35014f
ue64683 <gilles.diacon@sbb.ch>
2022-01-29 10:47:51 +0100

    gdi: improve partner layout

--- partners.php
+++ partners.php
@@ -1,15 +1,15 @@
-<div id="partner" class="<?php if('concept'==$activeMenuPage){ echo 'pt-8'; }else{ echo 'pt-3'; } ?> min-vh-100 container-fluid">
+<div id="partner" class="<?php if('concept'==$activeMenuPage){ echo 'pt-3'; }else{ echo 'pt-3'; } ?> container-fluid">
     <div class="row">
         <div class="col-lg-3 d-none d-lg-block">&nbsp;</div>
         <div class="col">
-            <div class="h1 pt-5 text-center">Partenaires</div>
+            <div class="h1 text-center">Partenaires</div>
             <div class="row justify-content-center">
                 <div class="col-lg-3 text-center">
                      <!-- <div class="h1 text-center text-nowrap">Ludesco</div> -->
                     <img src="image/partner/ludesco.png" height="80px">
                 </div>
                 <div class="col-lg-3 text-center">
-                    <div class="h4 text-nowrap pt-3">Escape Infinity</div>
+                    <div class="h4 text-nowrap pt-lg-3">Escape Infinity</div>
                 </div>
                 <div class="col-lg-3 text-center">
                     <!-- <div class="h1 text-center text-nowrap">(Clientis)</div> -->
@@ -17,7 +17,8 @@
                 </div>
                 <div class="col-lg-10 text-center">
                     <!-- <div class="h1 text-center text-nowrap">B12 Communication</div> -->
-                    <img src="image/partner/b12.png" height="40px">
+                    <img class="d-none d-lg-block" src="image/partner/b12.png" height="40px">
+                    <img class="d-lg-none" src="image/partner/b12_small.png" height="40px">
                 </div>
             </div>
         </div>
