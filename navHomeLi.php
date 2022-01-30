c179aa45d87401a965cb79e0f6c889eea52f74af
ue64683 <gilles.diacon@sbb.ch>
2022-01-30 16:42:58 +0100

    gdi: fix concept subMenu links

--- navHomeLi.php
+++ navHomeLi.php
@@ -1,15 +1,15 @@
     <li class="nav-item">
-        <a class="nav-link h5 py-0" href="#charte">Charte
+        <a class="nav-link h5 py-0" href="/concept.php#charte">Charte
 <!--        <img src="image/homeSubMenu/convention.svg" alt="Charte"/> -->
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link h5 py-0" href="#team">Equipe
+        <a class="nav-link h5 py-0" href="/concept.php#team">Equipe
 <!--        <img src="image/homeSubMenu/team.svg" alt="Equipe"/> -->
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link h5 py-0" href="#timetable">Horaires
+        <a class="nav-link h5 py-0" href="/concept.php#timetable">Horaires
 <!--        <img src="image/homeSubMenu/timetable.svg" alt="Horaires"/> -->
         </a>
     </li>
