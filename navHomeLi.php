a34f85d9f7dd09e0ce76518b16ca01c89a33d6dc
ue64683 <gilles.diacon@sbb.ch>
2022-02-02 07:52:31 +0100

    gdi: use new image for sub menu

--- navHomeLi.php
+++ navHomeLi.php
@@ -1,15 +1,9 @@
-    <li class="nav-item">
-        <a class="nav-link h5 py-0" href="/concept.php#charte">Charte
-<!--        <img src="image/homeSubMenu/convention.svg" alt="Charte"/> -->
-        </a>
+    <li class="nav-item px-2">
+        <a class="nav-link h5 py-0 m-0 amusebar-sub-menu amusebar-menu-convention" href="/concept.php#charte"></a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link h5 py-0" href="/concept.php#team">Equipe
-<!--        <img src="image/homeSubMenu/team.svg" alt="Equipe"/> -->
-        </a>
+    <li class="nav-item px-2">
+        <a class="nav-link h5 py-0 m-0 amusebar-sub-menu amusebar-menu-team" href="/concept.php#team"></a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link h5 py-0" href="/concept.php#timetable">Horaires
-<!--        <img src="image/homeSubMenu/timetable.svg" alt="Horaires"/> -->
-        </a>
+    <li class="nav-item px-2">
+        <a class="nav-link h5 py-0 m-0 amusebar-sub-menu amusebar-menu-timetable" href="/concept.php#timetable">Horaires</a>
     </li>
