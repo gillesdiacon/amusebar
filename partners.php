c4d27d779cb21395b61dd22d5ad85ec39af2fe42
ue64683 <gilles.diacon@sbb.ch>
2022-02-11 08:25:32 +0100

    gdi: replace clientis by exitLocus

--- partners.php
+++ partners.php
@@ -4,18 +4,18 @@
         <div class="col mx-lg-5 px-lg-4">
             <div class="h3 ps-sm-5">Partenaires</div>
             <div class="row justify-content-center">
-                <div class="col-lg-3 text-center">
+                <div class="col-lg-3 d-flex flex-wrap align-items-center justify-content-center">
                      <!-- <div class="h1 text-center text-nowrap">Ludesco</div> -->
                     <img src="image/partner/ludesco.png" height="80px">
                 </div>
-                <div class="col-lg-3 text-center">
-                    <div class="h4 text-nowrap pt-lg-3">Escape Infinity</div>
+                <div class="col-lg-3 d-flex flex-wrap align-items-center justify-content-center">
+                    <img src="image/partner/exitLocus.svg" height="120px">
                 </div>
-                <div class="col-lg-3 text-center">
+                <div class="col-lg-3 d-flex flex-wrap align-items-center justify-content-center">
                     <!-- <div class="h1 text-center text-nowrap">(Clientis)</div> -->
                     <img src="image/partner/clientis.png" height="80px">
                 </div>
-                <div class="col-lg-10 text-center">
+                <div class="col-lg-10 d-flex flex-wrap align-items-center justify-content-center">
                     <!-- <div class="h1 text-center text-nowrap">B12 Communication</div> -->
                     <img class="d-none d-lg-block" src="image/partner/b12.png" height="40px">
                     <img class="d-lg-none" src="image/partner/b12_small.png" height="40px">
