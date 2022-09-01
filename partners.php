1cca183fc4a3e70dc445f3c7ebc28b7fad763542
ue64683 <gilles.diacon@sbb.ch>
2022-09-01 13:11:44 +0200

    gdi: add lyceum partner

--- partners.php
+++ partners.php
@@ -1,4 +1,4 @@
-<div class="h3 text-center py-3 font-raleway fw-bold <?php if("_index" == $partnerLogoType) echo "text-white" ?>">Nos partenaires</div>
+<div class="h3 text-center py-3 font-raleway fw-bold <?php if('_index' == $partnerLogoType) echo 'text-white' ?>">Nos partenaires</div>
 <div class="row justify-content-center">
     <div class="col-lg-1 d-flex flex-wrap align-items-center justify-content-center">
         <img src="image/partner/ludesco<?php echo $partnerLogoType ?>.png" height="80px" alt="Ludesco est partenaire de l'Amuse-Bar à La Chaux-de-Fonds">
@@ -15,8 +15,15 @@
     <div class="col-lg-2 d-flex flex-wrap align-items-center justify-content-center py-3">
         <img src="image/partner/herosLocaux<?php echo $partnerLogoType ?>.png" height="60px" alt="Raiffaisen Heros Locaux est partenaire de l'Amuse-Bar à La Chaux-de-Fonds">
     </div>
-    <div class="col-lg-10 d-flex flex-wrap align-items-center justify-content-center py-3">
+    <div class="col-lg-5 d-flex flex-wrap align-items-center justify-content-center py-3">
         <img class="d-none d-lg-block" src="image/partner/b12<?php echo $partnerLogoType ?>.png" height="40px" alt="B12 est partenaire de l'Amuse-Bar à La Chaux-de-Fonds">
         <img class="d-lg-none" src="image/partner/b12_small.png" height="40px" alt="B12 est partenaire de l'Amuse-Bar à La Chaux-de-Fonds">
+    </div>
+    <div class="col-lg-2 d-flex flex-wrap align-items-center justify-content-center text-center py-3">
+        <div class="w-100">
+            <img src="image/partner/lyceum<?php echo $partnerLogoType ?>.png" height="70px" alt="Lyceum La Chaux-De-Fonds est partenaire de l'Amuse-Bar à La Chaux-de-Fonds">
+        </div>
+        <div class="fs-7 w-100 <?php if('_index' == $partnerLogoType) echo 'text-white' ?>">Lyceum Club International</div>
+        <div class="fs-7 w-100 <?php if('_index' == $partnerLogoType) echo 'text-white' ?>">La Chaux-de-Fonds</div>
     </div>
 </div>
