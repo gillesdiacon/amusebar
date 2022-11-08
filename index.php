b810c6ba0288b7bfa1c26310a05bd3bb4e61635b
ue64683 <gilles.diacon@sbb.ch>
2022-11-08 07:11:24 +0100

    gdi: add info about kitchen for sale

--- index.php
+++ index.php
@@ -31,8 +31,11 @@ require_once("init.php");
         </div>
 
         <div class="row justify-content-center text-center pt-3">
-            <div class="col-lg-6">
-                <img src="image/openning.png" width="500px" title="Ouverture décembre 2022 - nouvelle adresse - A l'Ancien Manège, rue du Manège 19, 2300 La Chaux-de-Fonds" />
+            <div class="col-lg-4">
+                <img src="image/openning.png" height="350px" title="Ouverture décembre 2022 - nouvelle adresse - A l'Ancien Manège, rue du Manège 19, 2300 La Chaux-de-Fonds" />
+            </div>
+            <div class="col-lg-4">
+                <img src="image/kitchenForSale.png" height="350px" title="Annonce - cuisine professionnelle à vendre" />
             </div>
         </div>
 <!--        <div class="py-5">&nbsp;</div>-->
