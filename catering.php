6f0673317d06fc2d437bf3ff25bb01e3e4516212
ue64683 <gilles.diacon@sbb.ch>
2023-01-06 06:56:39 +0100

    gdi: fix problem with punchline over links -> punchline is no more "fixed-botom"

--- catering.php
+++ catering.php
@@ -12,13 +12,13 @@ require_once("init.php");
     </head>
     <body>
 
-        <div class="fixed-bottom d-none d-lg-block">
-            <div class="row p-5">
-                <div class="col-lg-3">
-                    <img src="image/punchline/punchline-catering.svg" alt="L'Amuse-Bar, le bar qui ravit vos papilles et enchante votre palais" />
-                </div>
-            </div>
-        </div>
+<!--        <div class="fixed-bottom d-none d-lg-block">-->
+<!--            <div class="row p-5">-->
+<!--                <div class="col-lg-3">-->
+<!--                    <img src="image/punchline/punchline-catering.svg" alt="L'Amuse-Bar, le bar qui ravit vos papilles et enchante votre palais" />-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
 
         <div id="events" class="min-vh-100" style="background-image: url('image/bg/catering-filtre.png'); background-size: cover">
             <?php
@@ -26,26 +26,30 @@ require_once("init.php");
             ?>
             <div id="events" class="pb-4 px-lg-4 container-fluid">
                 <div class="row">
-                    <div class="col-lg-3 d-none d-lg-block">&nbsp;</div>
-                    <div class="col amuseBar-bg-cream mx-lg-5 px-4">
-                        <div class="h1 py-3 text-center"><?php echo $activeMenuTitle?></div>
+                    <div class="col-lg-3 d-none d-lg-block">
+                        <img class="pt-5" src="image/punchline/punchline-catering.svg" alt="L'Amuse-Bar, le bar qui ravit vos papilles et enchante votre palais" />
+                    </div>
+                    <div class="col mx-lg-5 p-0">
+                        <div class="amuseBar-bg-cream px-4">
+                            <div class="h1 py-3 text-center"><?php echo $activeMenuTitle?></div>
 
-                        <p>Notre carte de petite restauration vous propose des mets faits maison à partir de produits du terroir.
-                            Nous avons à coeur de respecter le cycle des saisons et de favoriser le plus possible la production suisse et locale.
-                            C'est pourquoi, nous avons testé pour vous différents produits pour sélectionner les meilleurs et les plus originaux.<br />
-                            Pour que chacun puisse avoir un plaisir gustatif, des recettes véganes vous seront également proposées.</p>
-                        <a target="_blank" href="catering/restaurationJanvier2023.pdf">
-                            <img src="image/food.png" height="50px" /> <!-- https://fontawesome.com/icons/user-chef?s=thin&f=classic -->
-                            <span>Carte restauration</span>
-                        </a>
+                            <p>Notre carte de petite restauration vous propose des mets faits maison à partir de produits du terroir.
+                                Nous avons à coeur de respecter le cycle des saisons et de favoriser le plus possible la production suisse et locale.
+                                C'est pourquoi, nous avons testé pour vous différents produits pour sélectionner les meilleurs et les plus originaux.<br />
+                                Pour que chacun puisse avoir un plaisir gustatif, des recettes véganes vous seront également proposées.</p>
+                            <a target="_blank" href="catering/restaurationJanvier2023.pdf">
+                                <img src="image/food.png" height="50px" /> <!-- https://fontawesome.com/icons/user-chef?s=thin&f=classic -->
+                                <span>Carte restauration</span>
+                            </a>
 
-                        <p class="mt-5">Au niveau de la carte des boissons, nous avons privilégié des bières artisanales, des cocktails originaux et des limonades et boissons maisons.</p>
-                        <a target="_blank" href="catering/boissonsJanvier2023.pdf">
-                            <img src="image/drink.png" height="50px" /> <!-- https://fontawesome.com/icons/glass-water?s=thin&f=classic -->
-                            <span>Carte boissons</span>
-                        </a>
+                            <p class="mt-5">Au niveau de la carte des boissons, nous avons privilégié des bières artisanales, des cocktails originaux et des limonades et boissons maisons.</p>
+                            <a target="_blank" href="catering/boissonsJanvier2023.pdf">
+                                <img src="image/drink.png" height="50px" /> <!-- https://fontawesome.com/icons/glass-water?s=thin&f=classic -->
+                                <span>Carte boissons</span>
+                            </a>
 
-                        <p>&nbsp;</p>
+                            <p>&nbsp;</p>
+                        </div>
                     </div>
                 </div>
             </div>
