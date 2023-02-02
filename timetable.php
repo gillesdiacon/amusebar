5b3eaa6e559603c7d805249a19cb305b1347e96a
ue64683 <gilles.diacon@sbb.ch>
2023-02-02 17:27:37 +0100

    gdi: timetable: update opening and closing date

--- timetable.php
+++ timetable.php
@@ -3,21 +3,25 @@
                 <td class="text-end">Lundi:</td>
                 <td class="text-center">15h30</td>
                 <td class="text-center">-</td>
-                <td class="text-center">23h00</td>
+                <td class="text-center">22h00</td>
                 <td class="px-2"></td>
             </tr>
-            <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
-                <td class="text-end">Mardi:</td>
-                <td class="text-center">15h30</td>
-                <td class="text-center">-</td>
-                <td class="text-center">23h00</td>
+            <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark border-bottom-0">
+                <td class="text-end" rowspan="2">Mardi:</td>
+                <td class="text-center border-bottom-0" colspan="3">fermé dès le 21 fév</td>
+                <td class="px-2 border-bottom-0"></td>
+            </tr>
+            <tr class="border-2 fw-normal border-top-0 border-start-0 border-end-0 border-dark">
+                <td class="text-center border-top-0">( 15h30</td>
+                <td class="text-center border-top-0">-</td>
+                <td class="text-center border-top-0">22h00 )</td>
                 <td class="px-2"></td>
             </tr>
             <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
                 <td class="text-end">Mercredi:</td>
                 <td class="text-center">11h30</td>
                 <td class="text-center">-</td>
-                <td class="text-center">23h00</td>
+                <td class="text-center">22h00</td>
                 <td class="px-2"></td>
             </tr>
             <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
@@ -36,16 +40,16 @@
             </tr>
             <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
                 <td class="text-end">Samedi:</td>
-                <td class="text-center">10h00</td>
+                <td class="text-center">10h30</td>
                 <td class="text-center">-</td>
-                <td class="text-center">02h00</td>
+                <td class="text-center">01h00</td>
                 <td class="px-2"></td>
             </tr>
             <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
                 <td class="text-end">Dimanche:</td>
-                <td class="text-center">10h00</td>
+                <td class="text-center">10h30</td>
                 <td class="text-center">-</td>
-                <td class="text-center">20h00</td>
+                <td class="text-center">19h00</td>
                 <td class="px-2"></td>
             </tr>
         </table>
