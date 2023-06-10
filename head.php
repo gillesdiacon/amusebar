56133d3e497caf6b521ce5734b29a4d25a4bda32
ue64683 <gilles.diacon@sbb.ch>
2023-06-10 11:52:28 +0200

    gdi: head: fix title tag

--- head.php
+++ head.php
@@ -4,7 +4,7 @@
         <meta name="author" content="">
         <link rel="icon" type="image/png" href="tp.png" />
     
-        <title><?php echo $activeMenuTitle; ?> | L&apos;AMUSE BAR</title>
+        <title><?php echo $activeMenuTitle; ?> | L&apos;AMUSE-BAR</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="css/amusebar.css" rel="stylesheet">
