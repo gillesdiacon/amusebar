47fc676a8dd5e989d80da3b88baa61f04e3a25dd
ue64683 <gilles.diacon@sbb.ch>
2023-06-10 10:43:59 +0200

    gdi: event: show event program for may

--- event.php
+++ event.php
@@ -33,12 +33,10 @@ require_once("init.php");
 
                 <div class="row">
                     <div class="col-lg-6 p-3">
-                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-202305.png" title="L'Amuse-Bar: animations du mois de mai 2023" />
-                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0 pt-lg-4" src="image/event-dragtatchaux.png" title="L'Amuse-Bar: animations Dragtat'chaux - show drag queen" />
+                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-202306-1.png" title="L'Amuse-Bar: animations du mois de juin 2023" />
                     </div>
                     <div class="col-lg-6 p-3">
-                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-202305_2.png" title="L'Amuse-Bar: animations du mois de mai 2023" />
-                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0 pt-lg-4" src="image/event-profchantage.png" title="L'Amuse-Bar: animations concert professeur chantage" />
+                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-202306-2.png" title="L'Amuse-Bar: animations du mois de juin 2023" />
                     </div>
                 </div>
 
