af7d71f11e6a828a364828f3554db24168494ba7
ue64683 <gilles.diacon@sbb.ch>
2023-11-02 07:08:56 +0100

    gdi: new Opening

--- timetable.php
+++ timetable.php
@@ -8,56 +8,61 @@
         <div class="ms-3 text-center">
             <table class="table border-dark border-2 fw-bold">
                 <tr class="border-2 border-start-0 border-end-0 border-dark">
-                    <td class="text-end">Lundi:</td>
-                    <td class="text-center">14h00</td>
-                    <td class="text-center">-</td>
-                    <td class="text-center">22h00</td>
-                    <td class="px-2"></td>
+                    <td class="text-end border-0">Lundi:</td>
+                    <td class="text-center border-0">15h00</td>
+                    <td class="text-center border-0">-</td>
+                    <td class="text-center border-0">22h00</td>
+                    <td class="px-2 border-0"></td>
                 </tr>
                 <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
-                    <td class="text-end">Mardi:</td>
-                    <td class="text-center border-bottom-0" colspan="3">fermé</td>
-                    <td class="px-2 border-bottom-0"></td>
+                    <td class="text-end border-0">Mardi:</td>
+                    <td class="text-center border-0" colspan="3">fermé</td>
+                    <td class="px-2 border-0"></td>
                 </tr>
 
                 <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
-                    <td class="text-end">Mercredi:</td>
-                    <td class="text-center">14h00</td>
-                    <td class="text-center">-</td>
-                    <td class="text-center">22h00</td>
-                    <td class="px-2"></td>
+                    <td class="text-end border-0">Mercredi:</td>
+                    <td class="text-center border-0">14h00</td>
+                    <td class="text-center border-0">-</td>
+                    <td class="text-center border-0">22h00</td>
+                    <td class="px-2 border-0"></td>
                 </tr>
                 <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
-                    <td class="text-end">Jeudi:</td>
-                    <td class="text-center">14h00</td>
-                    <td class="text-center">-</td>
-                    <td class="text-center">00h00</td>
-                    <td class="px-2"></td>
+                    <td class="text-end border-0">Jeudi:</td>
+                    <td class="text-center border-0">14h00</td>
+                    <td class="text-center border-0">-</td>
+                    <td class="text-center border-0">00h00</td>
+                    <td class="px-2 border-0"></td>
                 </tr>
                 <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
-                    <td class="text-end">Vendredi:</td>
-                    <td class="text-center">14h00</td>
-                    <td class="text-center">-</td>
-                    <td class="text-center">01h00</td>
-                    <td class="px-2"></td>
+                    <td class="text-end border-0">Vendredi:</td>
+                    <td class="text-cente border-0r">15h00</td>
+                    <td class="text-center border-0">-</td>
+                    <td class="text-center border-0">01h00</td>
+                    <td class="px-2 border-0"></td>
                 </tr>
                 <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
-                    <td class="text-end">Samedi:</td>
-                    <td class="text-center">10h30</td>
-                    <td class="text-center">-</td>
-                    <td class="text-center">01h00</td>
-                    <td class="px-2"></td>
+                    <td class="text-end border-0">Samedi:</td>
+                    <td class="text-center border-0">11h00</td>
+                    <td class="text-center border-0">-</td>
+                    <td class="text-center border-0">01h00</td>
+                    <td class="px-2 border-0"></td>
+                </tr>
+                <tr class="border-2 border-top-0 border-bottom-0 border-start-0 border-end-0 border-dark">
+                    <td class="text-end border-0 pb-0">Dimanche:</td>
+                    <td class="text-center border-0 pb-0">11h00</td>
+                    <td class="text-center border-0 pb-0">-</td>
+                    <td class="text-center border-0 pb-0">19h00</td>
+                    <td class="px-2 border-0 pb-0"></td>
                 </tr>
                 <tr class="border-2 border-top-0 border-start-0 border-end-0 border-dark">
-                    <td class="text-end">Dimanche:</td>
-                    <td class="text-center">10h30</td>
-                    <td class="text-center">-</td>
-                    <td class="text-center">19h00</td>
-                    <td class="px-2"></td>
+                    <td class="text-end border-0 pt-0">&nbsp;</td>
+                    <td class="text-center border-0 pt-0 fw-bold text-danger" colspan="3">Cuisine non stop</td>
+                    <td class="px-2 border-0 pt-0"></td>
                 </tr>
             </table>
 
-            <div class="py-2 fs-5 fw-bold text-danger text-center">ouverture à midi sur réservation</div>
+            <div class="py-2 fs-5 fw-bold text-danger text-center">Sur demande, nous ouvrons en dehors de ces horaires pour les groupes</div>
         </div>
 
 <!--        <div class="ms-3 text-center">-->
