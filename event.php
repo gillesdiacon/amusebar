03e3b5834d0ed7a5c72eb79151638bc85dc431ed
ue64683 <gilles.diacon@sbb.ch>
2023-11-02 06:50:21 +0100

    gdi: new Opening + remove halloween post

--- event.php
+++ event.php
@@ -41,24 +41,13 @@ require_once("init.php");
                         </a>
                     </div>
                     <div class="col-lg-6 p-3">
-                        <a href="image/event-halloween.mp4" target="_blank">
-                            <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-halloween23.png" title="L'Amuse-Bar: Venez fêter halloween à L'Amuse-Bar" />
-                        </a>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-lg-6 p-3">
                         <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-zorya.png" title="L'Amuse-Bar: J.K. Fournier présente son nouveau roman, Zorya" />
                     </div>
-                    <div class="col-lg-6 p-3">
-                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-fondueCartes.png" title="L'Amuse-Bar: Fondue + mach aux cartes les lundis 30 oct et 27 nov 2023" />
-                    </div>
                 </div>
 
                 <div class="row">
                     <div class="col-lg-6 p-3">
-                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-vendrediAperoQuiz.png" title="L'Amuse-Bar: Apéro Quiz tous les vendredis" />
+                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-fondueCartes.png" title="L'Amuse-Bar: Fondue + mach aux cartes les lundis 30 oct et 27 nov 2023" />
                     </div>
                     <div class="col-lg-6 p-3">
                         <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-lundiFondue.png" title="L'Amuse-Bar: Lundi fondue tous les lundis" />
@@ -67,11 +56,18 @@ require_once("init.php");
 
                 <div class="row">
                     <div class="col-lg-6 p-3">
+                        <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-vendrediAperoQuiz.png" title="L'Amuse-Bar: Apéro Quiz tous les vendredis" />
+                    </div>
+                    <div class="col-lg-6 p-3">
                         <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-jdr.png" title="L'Amuse-Bar: Jeux de Rôles 1 mercredi sur 2 - 18h à 20h" />
                     </div>
+                </div>
+
+                <div class="row">
                     <div class="col-lg-6 p-3">
                         <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-murderParty.png" title="L'Amuse-Bar: Murder Party" />
                     </div>
+                    <div class="col-lg-6 p-3">&nbsp;</div>
                 </div>
 
                 <div class="row">
--- index.php
+++ index.php
@@ -40,16 +40,14 @@ require_once("init.php");
                 </a>
             </div>
             <div class="col-lg-3">
-                <a target="_blank" href="image/event-halloween.mp4">
-                    <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-halloween23.png" title="L'Amuse-Bar: venez fêter halloween à l'Amuse-Bar" />
-                </a>
+                <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/newOpening.png" title="L'Amuse-Bar: Nouveaux horaires d'ouverture" />
+                <!-- <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-lundiFondue.png" title="L'Amuse-Bar: Fondue + mach aux cartes les lundis 30 oct et 27 nov 2023" /> -->
             </div>
         </div>
 
         <div class="row justify-content-center text-center pt-3">
             <div class="col-lg-3">
                 <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-zorya.png" title="L'Amuse-Bar: J.K. Fournier présente son nouveau roman, Zorya" />
-                <!-- <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/event-lundiFondue.png" title="L'Amuse-Bar: Fondue + mach aux cartes les lundis 30 oct et 27 nov 2023" /> -->
             </div>
             <div class="col-lg-3">
                 <a target="_blank" href="files/newsletter_2023_10.pdf">
