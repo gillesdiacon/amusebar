39471754bdfda740ccd8fb147d3ab02c0e785706
ue64683 <gilles.diacon@sbb.ch>
2024-03-06 17:18:26 +0100

    gdi: index: add new posts for march activities

--- index.php
+++ index.php
@@ -32,22 +32,20 @@ require_once("init.php");
 
         <div class="row justify-content-center text-center pt-3">
             <div class="col-lg-3">
-                <a target="_blank" href="https://mailchi.mp/672a5880548d/les-news-de-lamuse-bar-fvrier-2024">
-                    <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/newsletter_2024_02.png" title="L'Amuse-Bar: newsletter février 2024" />
+                <a target="_blank" href="https://mailchi.mp/e135522123fd/les-news-de-lamuse-bar-du-mois-de-mars">
+                    <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/newsletter_2024_03.png" title="L'Amuse-Bar: newsletter mars 2024" />
                 </a>
             </div>
             <div class="col-lg-3">
-                <video width="100%" controls poster="image/event-semaine7.png" title="L'Amuse-Bar: programme de la semaine du lundi 12 février">
-                    <source src="files/event-semaine7.mp4" type="video/mp4">
-                </video>
+                <a href="event-rolePlay.php">
+                    <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0 hover-border-primary-2" src="image/event-jdr.png" title="L'Amuse-Bar: Jeux de Rôles mercredi 6 et 20 mars - 18h à 22h" />
+                </a>
             </div>
         </div>
 
         <div class="row justify-content-center text-center pt-3">
             <div class="col-lg-3">
-                <video width="100%" controls poster="image/event-stValentin.png" title="L'Amuse-Bar: soirée speed gaming pour la St-Valentin">
-                    <source src="files/event-stValentin.mp4" type="video/mp4">
-                </video>
+                <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0 hover-border-primary-2" src="image/event-blindTest-live.png" title="L'Amuse-Bar: quiz musical en live avec Stay Awsome - 08 mars 2024" />
             </div>
             <div class="col-lg-3">
                 <video width="100%" controls poster="image/event-initiation-backgammon.png" title="L'Amuse-Bar: initiation au backgammon les 8, 15 et 22 février">
@@ -58,10 +56,16 @@ require_once("init.php");
 
         <div class="row justify-content-center text-center pt-3">
             <div class="col-lg-3">
-                <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0 hover-border-primary-2" src="image/event-impro17fev.jpeg" title="L'Amuse-Bar: capsule d'improviation théatrale - 17 février 2024" />
-<!--                <video width="100%" controls poster="image/event-blindTest-videoPoster.png">-->
-<!--                    <source src="files/event-blindTest.mp4" type="video/mp4">-->
-<!--                </video>-->
+                <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0 hover-border-primary-2" src="image/event-birthday-1.png" title="L'Amuse-Bar: anniversaire 1 an - 23 mars 2024" />
+            </div>
+            <div class="col-lg-3">
+                <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0 hover-border-primary-2" src="image/event-birthday-2.png" title="L'Amuse-Bar: anniversaire 1 an - 23 mars 2024" />
+            </div>
+        </div>
+
+        <div class="row justify-content-center text-center pt-3">
+            <div class="col-lg-3">
+                <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0 hover-border-primary-2" src="image/event-mondayFondueCards.png" title="L'Amuse-Bar: fondue - match aux cartes - 25 mars 2024" />
             </div>
             <div class="col-lg-3">
                 <img class="img-fluid p-3 p-sm-4 p-md-5 p-lg-0" src="image/shop-sale.png" title="L'Amuse-Bar: 20% sur toute la table du magasin de jeux" />
